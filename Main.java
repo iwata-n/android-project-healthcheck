ae2bf8c3045bf1bc25d604d362a674f692b8de4d
iwata naoki <a.tokai.wain@gmail.com>
2022-04-15 17:22:41 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Hello{
+  public void test() {
+    if (True) {
+      if (True) {
+        if (True) {
+          System.out.println("Hello");
+        }
+      }
+    }
+  }
+  public static void main(String[] args){
+    test()
+  }
+}
