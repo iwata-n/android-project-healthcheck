f97c2b9afe5e06a73da9c2769815d66fb4118fae
Naoki Iwata <naoki.iwata@zozo.com>
2022-04-15 17:34:40 +0900

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 mkdir result
-java -jar /checkstyle.jar -c ./result/checkstyle_rule.xml -o /checkstyle_result.txt **/*.java
-
 date > ./result/result.txt
 
-exit 0
+java -jar ./checkstyle.jar -c ./checkstyle_rule.xml -o ./result/checkstyle_result.txt **/*.java
