d481ac8c547b5819b38857d2d19750785824ad84
Naoki Iwata <naoki.iwata@zozo.com>
2022-04-20 17:57:41 +0900

    update

--- check.sh
+++ check.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
-java -jar $SCRIPT_PATH/checkstyle.jar -f xml -c $SCRIPT_PATH/checkstyle_rule.xml -o checkstyle_result.xml **/*.java
+mkdir $SCRIPT_PATH/result
+java -jar $SCRIPT_PATH/checkstyle.jar -f xml -c $SCRIPT_PATH/checkstyle_rule.xml -o $SCRIPT_PATH/result/checkstyle_result.xml **/*.java
 exit 0
